37ca42d7774a8028d5082d59cf58b9b0cd45de9e
Amrish kumar tiwary <166226420+Coderamrish@users.noreply.github.com>
2026-07-27 00:24:12 +0530

    leetcode 3

--- leetcode3.cpp
+++ leetcode3.cpp
@@ -22,26 +22,25 @@ s consists of English letters, digits, symbols and spaces.
 #include<algorithm>
 #include<sstream>
 #include<vector>
-#include<unordered_map>
 using namespace std;
 int lengthoflongestSubstring(string s) {
-    int n = s.length();
-    unordered_map<char, int>lastIndex;
-int left = 0, maxLen = 0;
-for(int right = 0; right < s.size(); right++) {
-    if(lastIndex.count(s[right]) && lastIndex[s[right]] >= left) {
-        left = lastIndex[s[right]] + 1;
-    }
-    lastIndex[s[right]] = right;
-    maxLen = max(maxLen, right - left + 1);
+    vector<int>lastIndex(256, -1);
+    int left = 0, maxLen = 0;
+    for (int right = 0; right < s.length(); right++) {
+unsigned char current = s[right];
+if(lastIndex[current] >= left) {
+    left = lastIndex[current] + 1;
 }
-return maxLen;
+lastIndex[current] = right;
+maxLen = max(maxLen, right - left + 1);
+    }
+    return maxLen;
 }
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     string s;
-    cin >> s;
+    getline(cin , s);
     cout << lengthoflongestSubstring(s) << "\n";
     return 0;
 }
