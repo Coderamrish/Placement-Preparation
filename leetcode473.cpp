ece077cda584c39bbd795d4ba5b0c01dcaab1d66
Amrish kumar tiwary <166226420+Coderamrish@users.noreply.github.com>
2026-07-27 11:50:35 +0530

    leetcode 1464 - maximum product of element in the array

---
+++ leetcode473.cpp
@@ -0,0 +1,49 @@
+/*
+Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+Example 1:
+Input: nums = [3,4,5,2]
+Output: 12 
+Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12. 
+Example 2:
+Input: nums = [1,5,4,5]
+Output: 16
+Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+Example 3:
+Input: nums = [3,7]
+Output: 12
+Constraints:
+2 <= nums.length <= 500
+1 <= nums[i] <= 10^3
+*/
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<climits>
+using namespace std;
+int maxProduct(vector<int>& nums) {
+    int n = nums.size();
+    int max1 = INT_MIN, max2 = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        if(nums[i] > max1) {
+            max2 = max1;
+            max1 = nums[i];
+        }
+        else if(nums[i] > max2) {
+            max2 = nums[i];
+        }
+    }
+    int result = (max1 - 1) * (max2 - 1);
+    return result;
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+    cout << maxProduct(nums) << "\n";
+    return 0;
+}
