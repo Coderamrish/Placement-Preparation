88b4b23f743779d616551d7c92c77207f2b1bc98
Amrish kumar tiwary <166226420+Coderamrish@users.noreply.github.com>
2026-09-09 19:37:25 +0530

    leetcode 3871

---
+++ leetcode517.cpp
@@ -0,0 +1,40 @@
+/*
+You are given an integer n.
+Return the total number of commas used when writing all integers from [1, n] (inclusive) in standard number formatting.
+In standard formatting:
+A comma is inserted after every three digits from the right.
+Numbers with fewer than 4 digits contain no commas.
+Example 1:
+Input: n = 1002
+Output: 3
+Explanation:
+The numbers "1,000", "1,001", and "1,002" each contain one comma, giving a total of 3.
+Example 2:
+Input: n = 998
+Output: 0
+Explanation:
+​​​​​​​All numbers from 1 to 998 have fewer than four digits. Therefore, no commas are used.
+Constraints:
+1 <= n <= 1015
+*/
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        for (long long x = 1000; x <= n; x *= 1000) {
+            ans += n - x + 1;
+        }
+        return ans;
+    }
+};
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long n;
+    cin >> n;
+    Solution obj;
+    cout << obj.countCommas(n) << '\n';
+    return 0;
+}
